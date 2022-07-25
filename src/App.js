ac82bdf7137314bfd87c754a66cba15e52c02444
sgujuluva <sanghee04@gmail.com>
2022-07-25 18:41:37 +0200

    search input and button is added in search component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+//components
+import Search from "./components/Search"
 //styles
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+     Weather
+     <Search/>
     </div>
   );
 }
---
+++ src/components/Search/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Search() {
+  return (
+    <div className="search">
+      <input type="search" placeholder="search city" />
+      <button>Search</button>
+    </div>
+  );
+}
+
+export default Search;
