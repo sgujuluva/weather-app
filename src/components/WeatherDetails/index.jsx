d56cc0fc1144928939475d7de91c773b0a60b1af
sgujuluva <sanghee04@gmail.com>
2022-07-31 06:40:08 +0200

    sun set rise info and humid info added

--- src/components/WeatherDetails/index.jsx
+++ src/components/WeatherDetails/index.jsx
@@ -1,27 +1,37 @@
-import React from 'react'
+import React from "react";
 //images import
-import sunny from "../../images/sunny.png"
+import sunny from "../../images/sunny.png";
+import sunRise from "../../images/sunrise.png";
+import humidity from "../../images/humidity.png";
 function WeatherDetails() {
   return (
     <>
-    <div className = "weather-icon">
-      <img src={sunny} alt="sun" />
-    </div>
-    <div className = "weather-info">
-        <div className = "temperature">
-        <span>25°</span>
+      <div className="weather-icon">
+        <img src={sunny} alt="sun" />
+      </div>
+      <div className="weather-info">
+        <div className="temperature">
+          <span>25°</span>
         </div>
         <div className="weather-description">
-            <div className = "weather-condition">Sunny</div>
-            <div className="city">Mumbai,IN</div>
+          <div className="weather-condition">Sunny</div>
+          <div className="city">Mumbai,IN</div>
         </div>
-        <div className="date-time">
-          {new Date().toLocaleString()}
+        <div className="date-time">{new Date().toLocaleString()}</div>
+      </div>
+      <div className="temp-details">
+        <div className="set-rise-info">
+         <img src={sunRise} alt="sun" />
+         <p>6:20 AM <br /> Sunrise</p>
         </div>
-    </div>
+        <div className="humidity">
+         <img src={humidity} alt="sun" />
+         <p>20 <br /> Humid</p>
+        </div>
+      </div>
+
     </>
-    
-  )
+  );
 }
 
-export default WeatherDetails
+export default WeatherDetails;
