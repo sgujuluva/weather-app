3309021b535c0b840f0bd9f50bd6aadaae269cb4
sgujuluva <sanghee04@gmail.com>
2022-08-02 19:06:21 +0200

    switch case written for all different weather conditions

--- src/components/WeatherDetails/index.jsx
+++ src/components/WeatherDetails/index.jsx
@@ -1,18 +1,45 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 //images import
 import sunny from "../../images/sunny.png";
 import sunRise from "../../images/sunrise.png";
 import humidityIcon from "../../images/humidity.png";
 import pressureIcon from "../../images/rain-pressure.png";
 import windIcon from "../../images/wind.png";
+import cloudyImg from "../../images/cloudy.png";
+import haze from "../../images/haze.png";
+import mist from "../../images/mist.png";
+import rainy from "../../images/rainy.png";
 
 function WeatherDetails({ tempInfo }) {
-  //
+  //for changing icons
   const [weatherState, setWeatherState] = useState("");
+  useEffect(() => {
+    if (tempInfo.weatherType) {
+      switch (tempInfo.weatherType) {
+        case "Clouds":
+          setWeatherState(cloudyImg);
+          break;
+        case "Haze":
+          setWeatherState(haze);
+          break;
+        case "Clear":
+          setWeatherState(sunny);
+          break;
+        case "Mist":
+          setWeatherState(mist);
+          break;
+        case "Rain":
+          setWeatherState(rainy);
+          break;
+        default:
+          setWeatherState(sunny);
+      }
+    }
+  }, [weatherState]);
   //converting the sec in min
   let sec = tempInfo.sunrise;
   let date = new Date(sec * 1000);
-  let timeStr = `${date.getHours()}:${date.getMinutes()}`
+  let timeStr = `${date.getHours()}:${date.getMinutes()}`;
   return (
     <>
       <div className="weather-icon">
@@ -25,8 +52,7 @@ function WeatherDetails({ tempInfo }) {
         <div className="weather-description">
           <div className="weather-condition">{tempInfo.weatherType}</div>
           <div className="city">
-            {tempInfo.name},
-            {tempInfo.country}
+            {tempInfo.name},{tempInfo.country}
           </div>
         </div>
         <div className="date-time">{new Date().toLocaleString()}</div>
